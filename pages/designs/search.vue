a80838fea0eb68efa4b789fdc7b9d5dacbf1291c
eg00 <egor@povshok.ru>
2021-03-01 01:35:51 +0500

    search designs results

--- pages/designs/search.vue
+++ pages/designs/search.vue
@@ -6,7 +6,12 @@
           <div class="filters d-flex justify-content-between align-items-center">
             <ul class="filters-dropdown">
               <li class="dropdown">
-                <select v-model="filters.orderBy" class="custom-select" style="width: 200px;">
+                <select
+                  v-model="filters.orderBy"
+                  @change="search"
+                  class="custom-select"
+                  style="width: 200px;"
+                >
                   <option value="latest">Latest First</option>
                   <option value="likes">Most Liked First</option>
                 </select>
@@ -60,93 +65,8 @@
     <section class="cards-block">
       <div class="container">
         <div class="row">
-          <div v-for="design in designs" :key="design.id"
-            class="w-100 col-sm-6 col-md-3 d-flex flex-column p-3"
-          >
-            <div
-              class="bg-white rounded shadow-sm overflow-hidden flex-1 flex flex-col p-1"
-            >
-              <a href="#">
-                <div
-                  class="bg-cover h-48"
-                  style="background-image: url('http://via.placeholder.com/250x200');"
-                >
-                  <div
-                    class="post-overlay p-4 d-flex flex-column align-items-center
-                    justify-content-center"
-                  >
-                    <h5
-                      class="fw-500 text-uppercase mb-2"
-                    >
-                      Short title
-                    </h5>
-                    <p class="font-12 lh-16">
-                      Short description here
-                    </p>
-                    <div
-                      class="card-datetime font-10 fw-300"
-                    >
-                      8 hours ago
-                    </div>
-                  </div>
-                </div>
-              </a>
-              <div class="p-1 d-flex flex-column">
-                <div
-                  class="mb-0 text-secondary d-flex justify-content-between align-items-center"
-                >
-                  <a href="#" class=" d-flex text-muted">
-                    <div>
-                      <img
-                        src="assets/images/profile.png"
-                        width="20"
-                        class="profile rounded mr-2"
-                      />
-                    </div>
-                    <h3
-                      class="card-title font-13 mt-1 mb-2 fw-400"
-                    >
-                      John Doe
-                      <span
-                        class="badge badge-pill badge-secondary text-white font-10 pr-1 pl-1 fw-300"
-                      >+ team</span
-                      >
-                    </h3>
-                  </a>
-                  <div class="mr-1 font-10 flex-2">
-                    8 hours ago
-                  </div>
-                </div>
-                <div
-                  class="card-datetime fw-300 d-flex justify-content-center"
-                >
-                  <div class="ml-1 text-right flex-1">
-                    <ul
-                      class="font-12 fw-400 list-inline text-light-gray"
-                    >
+          <design-card  v-for="design in designs" :key="design.id" :design="design"/>
 
-                      <li
-                        class="pt-0 list-inline-item"
-                      >
-                        <i
-                          class="fas fa-thumbs-up"
-                        ></i>
-                        2332
-                      </li>
-                      <li
-                        class="pt-0 list-inline-item"
-                      >
-                        <i
-                          class="fas fa-comments"
-                        ></i>
-                        299
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
         <!-- END ROW -->
       </div>
@@ -156,8 +76,11 @@
 </template>
 
 <script>
+import DesignCard from '~/components/_global/cards/_base-design';
+
 export default {
   name: 'search',
+  components: { DesignCard },
   data() {
     return {
       designs: [],
